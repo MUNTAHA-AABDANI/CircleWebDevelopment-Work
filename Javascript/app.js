7a7ec5d1201afa1aa05fd7ee0e531df5cb8a7ad2
Muntaha-Aabdani <https://github.com/Muntaha-Aabdani/Circle-web-development.git>
2021-05-23 01:21:40 -0700

    JavaScritp Function Implement

---
+++ Javascript/app.js
@@ -0,0 +1,10 @@
+ function printVariable(name)
+ {
+    let name1 = name;
+    console.log(name1);
+ }
+ console.log('Hello World');
+        var a = '10';
+        var v = "Muntaha";
+        console.log(v+a);
+    
